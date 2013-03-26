bacf1e05db0e11ad558feab6b6f0f378797f61da
Joel Sommers <jsommers@acm.org>
2013-03-26 13:21:06 -0400

    move run.sh to script folder; modify conf files for configuration core changes

---
+++ script/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONPATH=.:../pox:$PYTHONPATH
+# python -O fs.py -i1 -t60 conf/openflow1.dot 
+# python -O fs.py -i1 -t600 conf/ex_simple.dot
+# python -O fs.py -i1 -t600 conf/ex_pareto.dot
+
+# python -O fs.py -i1 -t600 conf/openflow_spf_small_harpoon.dot
+# python -O fs.py -i1 -t60 conf/openflow_spf_small_cbr.dot
+
+python -O fs.py -i1 -t60 test.conf

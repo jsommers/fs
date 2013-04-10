518361d3493376397541a09c1c3124af46910f0d
Joel Sommers <jsommers@acm.org>
2013-04-10 09:15:00 -0400

    Style updates

--- fs.py
+++ fs.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python
+'''
+Main class for fs: FsCore
+'''
 
 __author__ = 'jsommers@colgate.edu'
+
 
 import heapq
 import sys
 import signal
-import logging
-import time
 from optparse import OptionParser
 import configurator 
 import fscommon
 
 class FsCore(object):
+    '''Core simulation object --- handles event scheduling and
+    mediation between configuration and the classes that implement
+    simulation functionalities.'''
     inited = False
 
     def __init__(self, interval, endtime=1.0, debug=False, progtick=0.05):
         if FsCore.inited:
-            fscommon.get_logger().warn("Trying to initialize a new simulation object.")
+            fscommon.get_logger().warn(
+                    "Trying to initialize a new simulation object.")
             sys.exit(-1)
 
         self.__debug = debug
@@ -33,43 +39,56 @@ class FsCore(object):
         fscommon.set_fscore(self)
 
     def progress(self):
+        '''Callback for printing simulation timeline progress'''
         complete = (self.now - self.starttime) / float(self.endtime)
         self.logger.info('simulation completion: %2.2f' % (complete))
-        self.after(self.endtime*self.progtick, 'progress indicator', self.progress)
+        self.after(self.endtime*self.progtick, 
+            'progress indicator', self.progress)
 
     def sighandler(self, signum, stackframe):
+        '''Handle INT signal for shutting down simulation'''
         self.intr = True
 
     @property
     def topology(self):
+        '''Return the topology object'''
         return self.__topology
 
     @property
     def debug(self):
+        '''Return debug settings'''
         return self.__debug
 
     @property
     def now(self):
+        '''Get the current simulation time'''
         return self.__now
 
     @property  
     def logger(self):
+        '''Get the logger singleton object'''
         return self.__logger
 
     @property
     def interval(self):
+        '''Get the core simulation event interval'''
         return self.__interval
 
-    def after(self, delay, evid, fn, *fnargs):
+    def after(self, delay, evid, callback, *fnargs):
+        '''Schedule an event after delay seconds, identified by
+        evid (string), a callback function, and any necessary arguments
+        to the function'''
         expire_time = self.now + delay
-        heapq.heappush(self.__heap, (expire_time, evid, fn, fnargs))
+        heapq.heappush(self.__heap, (expire_time, evid, callback, fnargs))
 
     def run(self, scenario):
+        '''Start the simulation using a particular scenario filename'''
         cfg = configurator.FsConfigurator(self.debug)
         if scenario:
             self.__topology = cfg.load_config(scenario)
         else:
-            self.logger.info("No simulation scenario specified.  I'll just do nothing!")
+            self.logger.info("No simulation scenario specified." +
+                             "  I'll just do nothing!")
 
         self.after(0.0, 'progress indicator', self.progress)
 
@@ -78,28 +97,33 @@ class FsCore(object):
         while (self.__now - simstart) < self.endtime and not self.intr:
             if len(self.__heap) == 0:
                 break
-            expire_time,evid,fn,fnargs = heapq.heappop(self.__heap)
+            expire_time, evid, callback, fnargs = heapq.heappop(self.__heap)
             self.logger.debug("FS event: '{}'' @{}".format(evid, expire_time))
             self.__now = expire_time
-            fn(*fnargs)
+            callback(*fnargs)
             
-        self.logger.debug("Reached simulation end time: {}, {}".format(self.now, self.endtime))
+        self.logger.debug("Reached simulation end time: {}, {}"
+                .format(self.now, self.endtime))
         self.topology.stop()
 
 
 def main():
+    '''Parse command-line arguments and start up the simulation'''
     parser = OptionParser()
     parser.prog = "fs.py"
-    parser.add_option("-d", "--debug", dest="debug", default=False,
-                      action="store_true", help="Turn on debugging output")
-    parser.add_option("-t", "--simtime", dest="simtime", default=300, type=int,
-                      help="Set amount of simulation time; default=300 sec")
-    parser.add_option("-i", "--interval", dest="interval", default=1.0, type=float,
-                      help="Set the simulation tick interval (sec); default=1 sec")
+    parser.add_option("-d", "--debug", dest="debug", 
+                      default=False, action="store_true", 
+                      help="Turn on debugging output")
+    parser.add_option("-t", "--simtime", dest="simtime", 
+                      default=300, type=int,
+                      help="Set amount of simulation time")
+    parser.add_option("-i", "--interval", dest="interval", 
+                      default=1.0, type=float,
+                      help="Set the simulation tick interval (sec)")
     (options, args) = parser.parse_args()
 
     if len(args) != 1:
-        print >>sys.stderr,"Usage: %s [options] <scenario.dot>" % (sys.argv[0])
+        print >> sys.stderr,"Usage: %s [options] <scenario.dot>" % (sys.argv[0])
         sys.exit(0)
 
     sim = FsCore(options.interval, endtime=options.simtime, debug=options.debug)

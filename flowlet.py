33234e54ecb2a58c0744cbbe1bd0e8a5e3e69b99
Joel Sommers <jsommers@acm.org>
2013-04-27 20:43:28 -0400

    Add openflow stuff to a separate package

--- flowlet.py
+++ flowlet.py
@@ -4,12 +4,11 @@ __author__ = 'jsommers@colgate.edu'
 
 import copy
 import ipaddr
-import socket
+from socket import IPPROTO_TCP, IPPROTO_UDP, IPPROTO_ICMP
 import time
 from collections import namedtuple
 from fsutil import removeuniform
-from pox.openflow.libopenflow_01 import ofp_match
-import pox.openflow.libopenflow_01 as of
+
 
 class IncompatibleFlowlets(Exception):
     pass
@@ -80,7 +79,7 @@ class Flowlet(object):
     @property
     def endofflow(self):
         # check if tcp and FIN or RST
-        return self.ipproto == socket.IPPROTO_TCP and (self.tcpflags & 0x01 or self.tcpflags & 0x04)
+        return self.ipproto == IPPROTO_TCP and (self.tcpflags & 0x01 or self.tcpflags & 0x04)
 
     @property
     def key(self):
@@ -104,11 +103,11 @@ class Flowlet(object):
 
     @property
     def ipprotoname(self):
-        if self.ipproto == socket.IPPROTO_TCP:
+        if self.ipproto == IPPROTO_TCP:
             return 'tcp'
-        elif self.ipproto == socket.IPPROTO_UDP:
+        elif self.ipproto == IPPROTO_UDP:
             return 'udp'
-        elif self.ipproto == socket.IPPROTO_ICMP:
+        elif self.ipproto == IPPROTO_ICMP:
             return 'icmp'
         else:
             return 'ip'
@@ -247,130 +246,4 @@ class SubtractiveFlowlet(Flowlet):
         Flowlet.__init__(self, ident)
         self.action = action
 
-# Wrapper Class around POX for openflow messages
-class ofp_pox_messages:
-    __slots__ = ['pox_ofp_message']
-
-    def __init__(self, message_type, **kargs):
-        if message_type == 'ofp_packet_out':
-            self.pox_ofp_message = of.ofp_packet_out()
-            if 'action' in kargs:
-                if kargs['action'] == 'flood':
-                    self.pox_ofp_message.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-                elif kargs['action'] == 'ofpp_all':
-                    self.pox_ofp_message.actions.append(of.ofp_action_output(port = of.OFPP_ALL))
-
-        elif message_type == 'ofp_flow_mod':
-            if 'match' in kargs:
-                if 'command' in kargs:
-                    if kargs['command'] == 'add':
-                        self.pox_ofp_message = of.ofp_flow_mod(command=of.OFPFC_ADD, \
-                                                                   match=kargs['match'])
-                else:
-                    self.pox_ofp_message = of.ofp_flow_mod(match=kargs['match'])
-            else:
-                self.pox_ofp_message = of.ofp_flow_mod()
-            if 'match_dl_dst' in kargs:
-                self.pox_ofp_message.match.dl_dst = kargs['match_dl_dst']
-            if 'match_dl_src' in kargs:
-                self.pox_ofp_message.match.dl_src = kargs['match_dl_src']
-            if 'idle_timeout' in kargs:
-                self.pox_ofp_message.idle_timeout = kargs['idle_timeout']
-            if 'hard_timeout' in kargs:
-                self.pox_ofp_message.hard_timeout = kargs['hard_timeout']
-            if 'action' in kargs:
-                if kargs['action'] == 'flood':
-                    self.pox_ofp_message.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-                elif isinstance(kargs['action'], dict):
-                    if 'dstmac' in kargs['action'].keys():
-                        self.pox_ofp_message.actions.append(of.ofp_action_dl_addr.set_dst(\
-                                kargs['action']['dstmac']))
-                    if 'port' in kargs['action'].keys():
-                        self.pox_ofp_message.actions.append(of.ofp_action_output(port = kargs['action']['port']))
-                else:
-                    self.pox_ofp_message.actions.append(of.ofp_action_output(port = kargs['action']))
-            
-        elif message_type == 'ofp_packet_in':
-            self.pox_ofp_message = of.ofp_packet_in()
-            if 'reason' in kargs:
-                self.pox_ofp_message.reason = kargs['reason']
-            if 'in_port' in kargs:
-                self.pox_ofp_message.in_port = kargs['in_port']
-
-        elif message_type == 'ofp_flow_removed':
-            self.pox_ofp_message = of.ofp_flow_removed()
-            if 'match' in kargs:
-                self.pox_ofp_message.match = kargs['match']
-            if 'cookie' in kargs:
-                self.pox_ofp_message.cookie = kargs['cookie']
-            if 'priority' in kargs:
-                self.pox_ofp_message.priority = kargs['priority']
-            if 'reason' in kargs:
-                self.pox_ofp_message.reason = kargs['reason']
-            if 'duration_sec' in kargs:
-                self.pox_ofp_message.suration_sec = kargs['duration_sec']
-            if 'duration_nsec' in kargs:
-                self.pox_ofp_message.duration_nsec = kargs['duration_nsec']
-            if 'packet_count' in kargs:
-                self.pox_ofp_message.packet_count = kargs['packet_count']
-            if 'byte_count' in kargs:
-                self.pox_ofp_message.byte_count = kargs['byte_count']
-
-class OpenflowMessage(Flowlet):
-    __slots__ = ['context', 'message', 'message_type', 'data', 'actions']
-
-    def __init__(self, flowid, message_type, **kargs):
-        Flowlet.__init__(self, flowid)
-        # Creating class object of the message_type
-        self.message = ofp_pox_messages(message_type, **kargs) # e.g., ofp_packet_out, ofp_flow_mod, 
-        self.message_type = message_type
-        self.data = None
-        self.context = (None,None,None)
-
-    def set_context(self, origin, destination, previous):
-        self.context = (origin, destination, previous)
-
-    def get_context(self):
-        return self.context
-    
-    @property
-    def in_port(self):
-        if (self.message_type == 'ofp_packet_in'):
-            return self.message.pox_ofp_message.in_port
-
-    @property
-    def actions(self):
-        if (self.message_type == 'ofp_packet_out'):
-            return self.message.pox_ofp_message.actions
-
-    def __str__(self):
-        return "--".join([Flowlet.__str__(self), self.message_type])
-
-# Added by Joel
-# class OpenflowMessage(Flowlet):
-#     __slots__ = ['message','data']
-
-#     def __init__(self, flowid, message):
-#         Flowlet.__init__(self, flowid)
-#         self.message = message # e.g., object types ofp_packet_out, ofp_flow_mod
-#         self.data = None       # e.g., an attached data flowlet to go along with OF
-#                                # control message
-
-
-def ofp_match_from_flowlet(flowlet, ports=False):
-    m = ofp_match()
-    m.dl_src = flowlet.srcmac
-    m.dl_dst = flowlet.dstmac
-    m.dl_vlan = flowlet.vlan
-    m.nw_src = flowlet.srcaddr
-    m.nw_dst = flowlet.dstaddr
-    m.nw_proto = flowlet.ipproto
-    if ports:
-        m.tp_src = flowlet.srcport
-        m.tp_dst = flowlet.dstport
-    return m 
-    
-
-def flowident_from_ofp_match(m):
-    return FlowIdent(srcip=m.nw_src, dstip=m.nw_dst, ipproto=m.nw_proto, sport=m.tp_src, dport=m.tp_dst, srcmac=m.dl_src, dstmac=m.dl_dst, vlan=m.dl_vlan)
-    
+  

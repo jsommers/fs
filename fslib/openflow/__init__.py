c20147a4f3bce167db04bc06d12b9e43866a735f
Joel Sommers <jsommers@acm.org>
2013-05-05 07:36:32 -0400

    markdown-ify the README

--- fslib/openflow/__init__.py
+++ fslib/openflow/__init__.py
@@ -1,20 +1,6 @@
 # version 1 OpenflowSwitch and OpenflowController
-# from ofmessage_v1 import *
-# from ofnode_v1 import *
+from ofmessage_v1 import *
+from ofnode_v1 import *
 
-
-# version 2: bridge and delegate to POX
-
-import pox.core
-
-# FIXME
-def monkeypatcher(module):
-    '''Just because it's named monkeypatching doesn't mean it's all bad :-)'''
-    pass
-
-
-monkeypatcher()
-
-
-from pox_bridge import *
-
+# version 2: direct integration and monkeypatching of POX
+# currently in progress...

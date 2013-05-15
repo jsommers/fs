f7e6b5b63f500f047d674acb62e25da891c0cc6c
Joel Sommers <jsommers@acm.org>
2013-05-15 19:55:22 -0400

    profiling of/pox changes

--- script/profiler.py
+++ script/profiler.py
@@ -13,6 +13,7 @@ import fs
 sim = fs.FsCore(1, 10, debug=False)
 
 p = cProfile.Profile()
-p.run("sim.run('conf/simple_speed.json', configonly=False)")
+# p.run("sim.run('conf/simple_speed.json', configonly=False)")
+p.run("sim.run('test.dot', configonly=False)")
 p.print_stats(sort=1)
 
